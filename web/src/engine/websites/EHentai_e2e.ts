f43d54c042abe7dde85040f8d6215c1c52dc8dca
samueldovgin <s3dovgin@gmail.com>
2026-07-28 23:48:33 -0500

    Cover paginated E-Hentai galleries

--- web/src/engine/websites/EHentai_e2e.ts
+++ web/src/engine/websites/EHentai_e2e.ts
@@ -20,3 +20,24 @@ new TestFixture({
         type: 'image/webp',
     },
 }).AssertWebsite();
+
+new TestFixture({
+    plugin: {
+        id: 'ehentai',
+        title: 'E-Hentai',
+    },
+    container: {
+        url: 'https://e-hentai.org/g/4036064/8e2874a66c/',
+        id: '/g/4036064/8e2874a66c/',
+        title: '[C-O-Two- (Nayoshi)] Kokoro to Karada no Hogushiya-san | Mind & Body Relaxation [English] [Decensored]',
+    },
+    child: {
+        id: '/g/4036064/8e2874a66c/',
+        title: '[C-O-Two- (Nayoshi)] Kokoro to Karada no Hogushiya-san | Mind & Body Relaxation [English] [Decensored]',
+    },
+    entry: {
+        index: 48,
+        size: 91_784,
+        type: 'image/webp',
+    },
+}).AssertWebsite();
